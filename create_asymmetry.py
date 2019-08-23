ee0aadd4663a516b982ca0f59d632872a831b1df
Ziwei-Wu <840418303@qq.com>
2019-08-23 21:02:54 +0200

    Update create_asymmetry.py

--- create_asymmetry.py
+++ create_asymmetry.py
@@ -2,20 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import argparse
+import warnings
 
-# Using Argument Parser to get the location of image
-args = argparse.ArgumentParser()
-args.add_argument('-i', '--file', required=True, help='Path to image')
-arg = args.parse_args()
 
-filename = arg.file
-full_arr = np.genfromtxt('%s' % filename)
-#plt.imshow(full_arr[:,:],aspect='auto', origin='lower')
-#plt.colorbar()
-#plt.title('Original image')
-#plt.show()
-x, y = full_arr.shape
-print x,y
+def ignorewarning():
+    warnings.simplefilter('ignore', RuntimeWarning)
+    warnings.simplefilter('ignore', UserWarning)
+    warnings.simplefilter('ignore', FutureWarning)
+
+
  
 #1. find the noise level
 def findchange(data, start, gap, bin):
@@ -36,16 +31,10 @@ def findchange(data, start, gap, bin):
 
     return ymean_list,j 
 
-ymean_list, start = findchange(full_arr, 0, 20, 1)
-#plt.plot(ymean_list)
-#plt.plot([0,1000],[np.mean(ymean_list), np.mean(ymean_list)])
-#plt.title('determine noise level')
-#plt.show()
 
 def get_mean(full, bin):
     x, y = full.shape
     number = int(y/bin)
-    print number
     data = full[:,:]
     mean_list = []
     i = 0
@@ -55,6 +44,25 @@ def get_mean(full, bin):
     return mean_list
     
 
-mean_list = get_mean(full_arr[start:,:], 1)
-file_name = '%s_asymmetry.txt' % filename
-np.savetxt('%s' % file_name, mean_list[:-1],  fmt='%.5f')  
+def main():
+    # Using Argument Parser to get the location of image
+    args = argparse.ArgumentParser()
+    args.add_argument('-i', '--file', required=True, help='Path to image')
+    arg = args.parse_args()
+    filename = arg.file
+
+    full_arr = np.genfromtxt('%s' % filename)
+    print ('loading seocndary file: %s' % filename)
+    x, y = full_arr.shape
+
+    ymean_list, start = findchange(full_arr, 0, 20, 1)
+
+    mean_list = get_mean(full_arr[start:,:], 1)
+    file_name = '%s_asymmetry.txt' % filename
+    np.savetxt('%s' % file_name, mean_list[:-1],  fmt='%.5f')  
+
+if  __name__=="__main__":
+    ignorewarning()
+    main()
+
+    
